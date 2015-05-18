73f5f6a8ed8623ea3f28cc7f4a67b1ec13370fc5
Anne Noble <devney@stanford.edu>
2015-05-18 15:22:51 -0700

    first mockup for groceries

--- public/js/jst.js
+++ public/js/jst.js
@@ -4,10 +4,40 @@ window.JST = {};
 window.JST['landing'] = _.template(
 	'<div class="jumbotron">\
   		<h1>How\'s your economic citizenship?</h1>\
-  		<p>Let\'s do some errands to find out. What do you want to do?</p>\
+  		<p>Let\'s do some errands to find out. Where to?</p>\
   		<div class="btn-group-vertical">\
   			<button id="groceries-start" class="btn btn-lg">Get groceries.</button>\
   			<button id="bank-start" class="btn btn-lg">Go to the bank.</button>\
   		</div>\
 	</div>'
 	);
+
+
+window.JST['groceries'] = _.template(
+	'<div class="jumbotron">\
+  		<p>Where will you get groceries?</p>\
+  		<div class="btn-group-vertical" id="grocery-options-container">\
+  		</div>\
+	</div>'
+	);
+
+window.JST['grocery-option'] = _.template(
+		'<button id="<%=id%>" class="btn btn-lg"><%=desc%></button>'
+	);
+
+window.JST['groceries2'] = _.template(
+	'<div class="jumbotron">\
+		<div class="dropdown">\
+			<button class="btn btn-default dropdown-toggle" type="button" id="groceries-drop-down" data-toggle="dropdown" aria-expanded="true">\
+				Where will you get groceries?\
+				<span class="caret"></span>\
+			</button>\
+			<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">\
+				 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A local co-op</a></li>\
+				 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A local store with unknown labor and sourcing practices. </a></li>\
+				 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A large chain with good labor practices and local sourcing.</a></li>\
+				 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A large chain with unknown labor and sourcing practices</a></li>\
+			</ul>\
+		</div>\
+	</div>'
+);
--- public/js/main.js
+++ public/js/main.js
@@ -10,13 +10,74 @@
 	,	render :function(){
 			var landing_template = window.JST["landing"]
 			$(this.el).append(landing_template);
-			$("#groceries-start").click(this.getGroceries());
+			$("#groceries-start").click(this.getGroceries);
 		}
 	,	getGroceries :function(){
 			console.log("heading out for groceries")
 			app.getGroceries();
-	}
+		}
 	});
+
+	var GroceriesView = Backbone.View.extend({
+		el: '#teaser-container'
+	,	initialize:function(){
+			_.bindAll(this, 'render');
+			this.render = _.wrap(this.render, function(render) {	//keeps 'this' this in afterRender
+                render();
+                this.afterRender();
+            });
+			this.render();
+		}
+	,	render: function(){
+			$(this.el).html(''); //clear
+			var groceries_template = window.JST["groceries"]
+			$(this.el).append(groceries_template);		
+		}
+	,	afterRender:function(){
+			var that = this;
+			var grocery_options = {
+				"3" : "A local co-op"
+			,	"2"	: "A grocery with good labor practices and local sourcing"
+			,	"1" : "A big-name grocery with unknown labor practices and the most convenient sourcing."
+			}
+			var keys = Object.keys(grocery_options);
+			_.each(keys, function(option_id, i, list){
+				var template = window.JST["grocery-option"]
+				var id = "grocery-option-" + option_id;
+				$("#grocery-options-container").append(template({id: id, desc: grocery_options[option_id]}))
+				var selector = "#" + id;
+				$(selector).click({option: option_id}, that.getResult)
+			})
+		}
+	,	getResult: function(e){
+			e.preventDefault();
+			var score =  e.data["option"];
+			console.log("Option selected: " + e.data["option"]);
+			app.getResult(score);
+		}
+	});
+
+
+
+	var ResultsView = Backbone.Router.extend({
+		initialize: function(options){
+			this.options = options;
+			_.bindAll(this, 'render');
+			this.render();
+			
+		}
+	,	render:function(){
+			var score = this.options.score
+			var groceries_scoring = {
+				"1": "You are beginning your journey to economic citizenship."
+			,	"2": "You are a builder!"
+			,	"3": "You are a change-maker!"
+			}
+			alert("Your score is: " + score  + " out of 3. " + groceries_scoring[score]);
+		}
+	});
+
+	
 
 	var AppRouter = Backbone.Router.extend({
 		routes: {
@@ -27,7 +88,12 @@
 		}
 	,	getGroceries: function(){
 			console.log("and we're off for groceries!")
+			this.groceries_view = new GroceriesView();
 	}
+	,	getResult: function(score){
+			this.results_view = new ResultsView({score: score});
+
+		}
 	});
 	//start the app
 	
